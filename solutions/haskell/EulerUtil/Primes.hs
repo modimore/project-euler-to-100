88758bf29fb241dc567548d05fc5a9edfada817b
Quinn Modimore <quinn.modimore@gmail.com>
2018-06-11 18:48:08 -0400

    Adding infinite prime sequence in Haskell
    Not too fast, but infinite at least.

--- solutions/haskell/EulerUtil/Primes.hs
+++ solutions/haskell/EulerUtil/Primes.hs
@@ -1,11 +1,9 @@
 module EulerUtil.Primes (
+    primes,
     primesTo
 ) where
 
--- Generate primes up to m
-primesTo :: Int -> [Int]
-primesTo m
-    | m <  2 = []
-    | m == 2 = []
-    | otherwise = 2 : sieve [3,5..m]
-        where sieve [] = []; sieve (x:xs) = x : sieve (filter (\n-> mod n x /= 0) xs)
+primes = 2 : sieve [3,5..]
+    where sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
+
+primesTo m = takeWhile (<=m) primes
